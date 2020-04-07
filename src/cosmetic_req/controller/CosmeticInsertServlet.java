b2a91129c6514b42940c7f2d864dcc1b5a5ea4b6
kleg26315 <kleg26315@naver.com>
2020-04-08 01:13:14 +0900

    화장품등록 이미지까지 완료

--- src/cosmetic_req/controller/CosmeticInsertServlet.java
+++ src/cosmetic_req/controller/CosmeticInsertServlet.java
@@ -1,6 +1,7 @@
 package cosmetic_req.controller;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.oreilly.servlet.MultipartRequest;
+
+import common.CosReqFileRenamePolicy;
 import cosmetic.model.vo.Cosmetic;
 import cosmetic_req.model.service.Cosmetic_reqService;
 
@@ -32,27 +38,54 @@ public class CosmeticInsertServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		String brandName = request.getParameter("brandName");
-		String middleCg = request.getParameter("middleCg");
-		String name = request.getParameter("name");
-		String content = request.getParameter("content");
-		String volume = request.getParameter("volume");
-		String price = request.getParameter("price");
-		Cosmetic c = new Cosmetic(0, name, content, volume, price, 0, 0, brandName, middleCg, "임시");
 		
-		int result = new Cosmetic_reqService().insertCos(c);
-		
-		String page = null;
-		if(result > 0) {
-			page = "views/admin/cosmeticReqPopup.jsp";
-			request.setAttribute("result", result);
-		} else {
-			page = "views/common/errorPage.jsp";
-			request.setAttribute("msg", "화장품 등록에 실패하였습니다.");
+		if(ServletFileUpload.isMultipartContent(request)) {
+			int maxSize = 1024 * 1024 * 10;
+			String root = request.getSession().getServletContext().getRealPath("/");
+			String savePath = root + "cosReq_uploadFiles/";
+			
+			MultipartRequest multiRequest
+				= new MultipartRequest(request, savePath, maxSize, "UTF-8", new CosReqFileRenamePolicy());
+			
+			String saveFile = null;
+			Enumeration<String> files = multiRequest.getFileNames();
+			while(files.hasMoreElements()) {
+				String name = files.nextElement();
+				
+				if(multiRequest.getFilesystemName(name) != null) {
+					saveFile = multiRequest.getFilesystemName(name);
+				}
+			}
+			
+			String brandName = multiRequest.getParameter("brandName");
+			String middleCg = multiRequest.getParameter("middleCg");
+			String name = multiRequest.getParameter("name");
+			String content = multiRequest.getParameter("content");
+			String volume = multiRequest.getParameter("volume");
+			String price = multiRequest.getParameter("price");
+			Cosmetic c = new Cosmetic();
+			c.setBrand_name(brandName);
+			c.setMiddle_no(middleCg);
+			c.setCosmetic_name(name);
+			c.setCosmetic_about(content);
+			c.setVolume(volume);
+			c.setPrice(price);
+			c.setCosmetic_img(saveFile);
+			
+			int result = new Cosmetic_reqService().insertCos(c);
+			
+			String page = null;
+			if(result > 0) {
+				page = "views/admin/cosmeticReqPopup.jsp";
+				request.setAttribute("result", result);
+			} else {
+				page = "views/common/errorPage.jsp";
+				request.setAttribute("msg", "화장품 등록에 실패하였습니다.");
+			}
+			
+			RequestDispatcher view = request.getRequestDispatcher(page);
+			view.forward(request, response);
 		}
-		
-		RequestDispatcher view = request.getRequestDispatcher(page);
-		view.forward(request, response);
 	}
 
 	/**
