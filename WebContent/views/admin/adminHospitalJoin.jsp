b85b7b014aa4cc9114932c5d7158a14f9cb092b0
김경섭(Kim KyouongSub) <kleg26315@naver.com>
2020-04-22 19:15:19 +0900

    관리자페이지 게시판 관리 완료

--- WebContent/views/admin/adminHospitalJoin.jsp
+++ WebContent/views/admin/adminHospitalJoin.jsp
@@ -181,7 +181,7 @@ table#adminEnrollHospital>tbody>tr>td{
                <li style="background: #f2d0e0" onclick="location.href = '<%= request.getContextPath()%>/hospitalJoin.admin'">병원 가입 관리</li>
                <li onclick="location.href = '<%= request.getContextPath()%>/views/admin/adminCosReq.jsp'">제품 등록 관리</li>
                <li onclick="location.href = '<%= request.getContextPath()%>/reviewList.admin'">리뷰 관리</li>
-               <li onclick="location.href = '<%= request.getContextPath()%>/views/admin/adminBoard.jsp'">게시판 관리</li>
+               <li onclick="location.href = '<%= request.getContextPath()%>/boardList.admin'">게시판 관리</li>
                <li onclick="location.href = '<%= request.getContextPath()%>/views/admin/adminInq.jsp'">1대1문의 관리</li>
             </ul>
          </section>
---
+++ src/admin/controller/BoardDeleteServlet.java
@@ -0,0 +1,45 @@
+package admin.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import worry.model.service.WorryService;
+
+/**
+ * Servlet implementation class BoardDeleteServlet
+ */
+@WebServlet("/worryDelete.admin")
+public class BoardDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int worryNo = Integer.parseInt(request.getParameter("worryNo"));
+		int result = new WorryService().deleteWorry(worryNo);
+		
+		response.sendRedirect("boardList.admin");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
